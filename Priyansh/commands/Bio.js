c4ba4aa07a91483be85f2983f5c492051e91425f
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-02 18:15:35 +0200

    Update Bio.js

--- Priyansh/commands/Bio.js
+++ Priyansh/commands/Bio.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-	name: "bio",
+	name: "بايو",
 	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
@@ -12,7 +12,7 @@ module.exports.config = {
   
   module.exports.run = async ({ api, event, global, args, permssion, utils, client, Users }) => {
     api.changeBio(args.join(" "), (e) => {
-      if(e) api.sendMessage("an error occurred" + e, event.threadID); return api.sendMessage("Has changed the biography of the bot into: \n"+args.join(" "), event.threadID, event.messgaeID)
+      if(e) api.sendMessage("an error occurred" + e, event.threadID); return api.sendMessage("هذا البايو الجديد للبوت : \n"+args.join(" "), event.threadID, event.messgaeID)
     }
     )
   }
