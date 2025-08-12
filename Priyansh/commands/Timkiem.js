70c02879c905c4e30e63c3fc8da1d8b8a0b41a3a
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 22:40:51 +0200

    Update Timkiem.js

--- Priyansh/commands/Timkiem.js
+++ Priyansh/commands/Timkiem.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-    name: "search",
+    name: "بحث",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "Search results on google",
+    description: "البحث في جوجل",
     commandCategory: "info",
-    usages: "search [Text]",
+    usages: "search [نص]",
     cooldowns: 5,
     dependencies: {
         "request":"",
@@ -16,6 +16,15 @@ module.exports.config = {
 module.exports.run = function({ api, event, args }) {
     let textNeedSearch = "";
     const regex = /(https?:\/\/.*?\.(?:png|jpe?g|gif)(?:\?(?:[\w_-]+=[\w_-]+)(?:&[\w_-]+=[\w_-]+)*)?(.*))($)/;
-    (event.type == "message_reply") ? textNeedSearch = event.messageReply.attachments[0].url: textNeedSearch = args.join(" ");
-    (regex.test(textNeedSearch)) ? api.sendMessage(`https://www.google.com/searchbyimage?&image_url=${textNeedSearch}`, event.threadID, event.messageID): api.sendMessage(`https://www.google.com.vn/search?q=${encodeURIComponent(textNeedSearch)}`, event.threadID, event.messageID);
+
+    // لو الرسالة رد على صورة ناخذ رابطها، وإلا ناخذ النص المكتوب
+    (event.type == "message_reply") ? textNeedSearch = event.messageReply.attachments[0].url : textNeedSearch = args.join(" ");
+
+    if (!textNeedSearch) return api.sendMessage("❌  الرجاء ارسال نص او الرد على صورة للبحث 🔍", event.threadID, event.messageID);
+
+    if (regex.test(textNeedSearch)) {
+        api.sendMessage(`🔎  هذا رابط البحث عن الصورة: \nhttps://www.google.com/searchbyimage?&image_url=${textNeedSearch}`, event.threadID, event.messageID);
+    } else {
+        api.sendMessage(`🔎  نتائج البحث عن: « ${textNeedSearch} » \nhttps://www.google.com.vn/search?q=${encodeURIComponent(textNeedSearch)}`, event.threadID, event.messageID);
+    }
 }
