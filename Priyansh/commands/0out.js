bc8ebe4266075f2ebe285c973c7afc59047253c0
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 22:24:45 +0200

    Update 0out.js

--- Priyansh/commands/0out.js
+++ Priyansh/commands/0out.js
@@ -1,15 +1,19 @@
 module.exports.config = {
-    name: "out",
+    name: "غادري",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "",
-    commandCategory: "Admin",
-    usages: "out [id]",
+    credits: "Priyansh Rajput",
+    description: " أمر خروج البوت من المجموعة",
+    commandCategory: "الإدارة",
+    usages: "out [معرف_المجموعة]",
     cooldowns: 10,
 };
 
 module.exports.run = async function({ api, event, args }) {
-        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
-}
+    if (!args[0]) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+    }
+    if (!isNaN(args[0])) {
+        return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+    }
+};
