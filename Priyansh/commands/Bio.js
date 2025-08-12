d92921d7b7ff40a6ad1834bfc92b37af04d07195
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 22:31:21 +0200

    Update Bio.js

--- Priyansh/commands/Bio.js
+++ Priyansh/commands/Bio.js
@@ -1,18 +1,18 @@
 module.exports.config = {
-	name: "bio",
-	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Change bot's bio",
-	commandCategory: "admin",
-	usages: "bio [text]",
+  name: "بايو",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "Priyansh Rajput",
+  description: "تغيير نبذة البوت",
+  commandCategory: "admin",
+  usages: "bio [النص]",
   cooldowns: 5
-  
 }
-  
-  module.exports.run = async ({ api, event, global, args, permssion, utils, client, Users }) => {
-    api.changeBio(args.join(" "), (e) => {
-      if(e) api.sendMessage("an error occurred" + e, event.threadID); return api.sendMessage("Has changed the biography of the bot into: \n"+args.join(" "), event.threadID, event.messgaeID)
-    }
-    )
-  }
+
+module.exports.run = async ({ api, event, args }) => {
+  api.changeBio(args.join(" "), (error) => {
+    if(error) 
+      return api.sendMessage("❌ حدث خطأ: " + error, event.threadID);
+    return api.sendMessage("✔️ تم تغيير النبذة إلى:\n" + args.join(" "), event.threadID, event.messageID);
+  });
+	}
