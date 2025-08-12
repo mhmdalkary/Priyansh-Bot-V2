ada400c9b1080bcc1afe6ad601b8c39659746f1a
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 22:19:29 +0200

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-	name: "info",
+	name: "معلومات",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Admin and Bot info.",
-	commandCategory: "...",
+	credits: "Priyansh Rajput",
+	description: "معلومات عن الأدمن والبوت",
+	commandCategory: "معلومات",
 	cooldowns: 1,
 	dependencies: 
 	{
@@ -13,50 +13,49 @@ module.exports.config = {
     "axios":""
   }
 };
+
 module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
+	const axios = global.nodemodule["axios"];
+	const request = global.nodemodule["request"];
+	const fs = global.nodemodule["fs-extra"];
+	const time = process.uptime(),
 		hours = Math.floor(time / (60 * 60)),
 		minutes = Math.floor((time % (60 * 60)) / 60),
 		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.imgur.com/eDbdlvd.jpg"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
+	const moment = require("moment-timezone");
+	var currentTime = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+	var images = ["https://i.imgur.com/eDbdlvd.jpg"];
 
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
+	var sendMessage = () => api.sendMessage({
+		body: `
+✿❀ 『 معلومات الأدمن والبوت 』 ❀✿
 
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙💔🥀
+➤ اسم البوت: 『 ${global.config.BOTNAME} 』
 
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/priyanshu.rajput.official 💞🕊️
+➤ صاحب البوت: 『 Priyansh Rajput 』
 
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇
+➤ رابط فيسبوك الأدمن: 『 www.facebook.com/priyanshu.rajput.official 』
 
-✧══════•❁❀❁•══════✧
+➤ للتواصل عبر تليجرام: 『 @Priyanshrajput 』
 
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+•••••••••••••••••••••••••••••••••
 
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭
+✧ بادئة البوت ✧ 『 ${global.config.PREFIX} 』
 
-🥳UPTIME🥳
+✧ صاحب البوت ✧ 『 Priyansh Rajput 』
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+✧ مدة تشغيل البوت ✧
+『 ${hours} ساعة : ${minutes} دقيقة : ${seconds} ثانية 』
 
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
+✧ التاريخ والوقت الحالي ✧
+『 ${currentTime} 』
 
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
+✧ شكراً لاستخدامك بوت 『 ${global.config.BOTNAME} 』
 
+➤⊹
+		`,
+		attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")
+	}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg"));
 
-🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🌿✰✧✧✧✧✦🌸
-┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
-
-
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
-   };
+	return request(encodeURI(images[Math.floor(Math.random() * images.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close", () => sendMessage());
+};
