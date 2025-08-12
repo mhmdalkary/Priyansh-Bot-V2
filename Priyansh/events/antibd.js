b9d65d243f83df431129dff9b78bc59c777d4f30
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 17:36:16 +0200

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -1,20 +1,29 @@
 module.exports.config = {
-  name: "antibd",
-  eventType: ["log:user-nickname"],
-  version: "0.0.1",
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-  description: "Against changing Bot's nickname"
+    name: "antibd",
+    eventType: ["log:user-nickname"],
+    version: "0.0.1",
+    credits: "Priyansh Rajput",
+    description: "منع تغيير لقب البوت"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
-    }  
-        }
+    const { logMessageData, threadID, author } = event;
+    const botID = api.getCurrentUserID();
+    const { BOTNAME, ADMINBOT } = global.config;
+    let { nickname } = await Threads.getData(threadID, botID);
+    nickname = nickname || BOTNAME;
+
+    if (logMessageData.participant_id === botID &&
+        author !== botID &&
+        !ADMINBOT.includes(author) &&
+        logMessageData.nickname !== nickname) {
+
+        api.changeNickname(nickname, threadID, botID);
+
+        const info = await Users.getData(author);
+        return api.sendMessage(
+            `✧✦ تحذير ✦✧\n『 ${info.name} 』\n➤⊹ لا يمكنك تغيير لقب البوت!`,
+            threadID
+        );
+    }
+};
