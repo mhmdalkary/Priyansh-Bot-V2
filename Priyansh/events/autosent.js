6cd9e1a4fe28482f0485dcd3fb2dc4d113c3673d
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 17:31:42 +0200

    Update autosent.js

--- Priyansh/events/autosent.js
+++ Priyansh/events/autosent.js
@@ -1,27 +1,33 @@
 module.exports.config = {
- name: "autosend",
- eventType: [],
- version: "0.0.1",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
- description: "Listen events"
+    name: "autosend",
+    eventType: [],
+    version: "0.0.1",
+    credits: "Priyansh Rajput",
+    description: "متابعة الأحداث وإرسال رسائل تلقائية"
 };
 
-module.exports.run = async({ event, api, Threads, Users }) => {
-const moment = require("moment-timezone");
-time = moment.tz('Asia/Kolkata').format('HH:mm:ss');
-var cantsend = [];
-    var allThread = global.data.allThreadID || [];
-    if (time == "16:52:00") {
-    for (const idThread of allThread) {
-        if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
-        else {
-            api.sendMessage("test" + args.join(" ") , idThread, (error, info) => {
-                if (error) cantsend.push(idThread);
-            });
+module.exports.run = async ({ event, api, Threads, Users, args }) => {
+    const moment = require("moment-timezone");
+    const time = moment.tz('Asia/Kolkata').format('HH:mm:ss');
+    let cantsend = [];
+    const allThread = global.data.allThreadID || [];
+
+    if (time === "16:52:00") {
+        for (const idThread of allThread) {
+            if (!isNaN(parseInt(idThread)) && idThread !== event.threadID) {
+                api.sendMessage(`رسالة اختبار ${args ? args.join(" ") : ""}`, idThread, (error) => {
+                    if (error) cantsend.push(idThread);
+                });
+            }
         }
-      }
-    for (var id of global.config.ADMINBOT) {
-          api.sendMessage(`Error when automatically sending messages to threads:\n${cantsend.join("\n")}`,id);
+
+        for (const adminID of global.config.ADMINBOT) {
+            if (cantsend.length > 0) {
+                api.sendMessage(
+                    `خطأ أثناء الإرسال التلقائي للرسائل في المجموعات التالية:\n${cantsend.join("\n")}`,
+                    adminID
+                );
+            }
+        }
     }
-  }
-                                                                                          }
+};
