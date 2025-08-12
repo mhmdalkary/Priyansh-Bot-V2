39035b28d2184f5dbea9f4777f4dd475be6e8d6d
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-13 00:08:08 +0200

    Update antirobbery.js

--- Priyansh/commands/antirobbery.js
+++ Priyansh/commands/antirobbery.js
@@ -1,22 +1,25 @@
 module.exports.config = {
- name: "antirobbery",
- version: "1.0.0",
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
- hasPermssion: 1,
- description: "Prevent changing group administrators",
- usages: "",
- commandCategory: "Box Chat",
- cooldowns: 0
+    name: "منع",
+    version: "1.0.0",
+    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    hasPermssion: 1,
+    description: "منع تغيير مشرفين المجموعة",
+    usages: "",
+    commandCategory: "إدارة المجموعات",
+    cooldowns: 0
 };
 
-module.exports.run = async({ api, event, Threads}) => {
+module.exports.run = async ({ api, event, Threads }) => {
     const info = await api.getThreadInfo(event.threadID);
     if (!info.adminIDs.some(item => item.id == api.getCurrentUserID())) 
-      return api.sendMessage('Need group administrator permissions, please add and try again!', event.threadID, event.messageID);
+        return api.sendMessage('❌ لازم البوت يكون مشرف بالمجموعة، ضيفه كمشرف وحاول مرة ثانية', event.threadID, event.messageID);
+
     const data = (await Threads.getData(event.threadID)).data || {};
-    if (typeof data["guard"] == "guard" || data["guard"] == false) data["guard"] = true;
+    if (typeof data["guard"] == "undefined" || data["guard"] == false) data["guard"] = true;
     else data["guard"] = false;
+
     await Threads.setData(event.threadID, { data });
-      global.data.threadData.set(parseInt(event.threadID), data);
-    return api.sendMessage(`${(data["guard"] == true) ? "turn on" : "Turn off"} Successful Anti-Robbery Group`, event.threadID, event.messageID);
-}
+    global.data.threadData.set(parseInt(event.threadID), data);
+
+    return api.sendMessage(`✅ تم ${(data["guard"] == true) ? "تفعيل" : "إيقاف"} حماية المشرفين بنجاح`, event.threadID, event.messageID);
+};
