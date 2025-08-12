1bb7f2dd5a4b645be5a168b36446558a80c5b8d6
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-13 00:17:17 +0200

    Update ara.js

--- Priyansh/commands/ara.js
+++ Priyansh/commands/ara.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "araara",
+	name: "ارا",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   let bot = global.config.OTHERBOT;
 	
-	if ((event.body.indexOf("ara ara")==0 || event.body.indexOf("Ara ara")==0 || event.body.indexOf("ara")==0 || event.body.indexOf("Ara")==0 ) && !bot.includes(event.senderID)) {
+	if ((event.body.indexOf("ارا ارا")==0 || event.body.indexOf("اراا")==0 || event.body.indexOf("ارا")==0 || event.body.indexOf("Ara")==0 ) && !bot.includes(event.senderID)) {
 		var msg = {
 				body: "Ara ara~",
 				attachment: fs.createReadStream(__dirname + `/noprefix/ara.mp3`)
