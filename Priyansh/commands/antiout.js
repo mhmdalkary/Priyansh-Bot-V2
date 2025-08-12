9764c68a270c238f3234f2b27880e2a2b748c0f1
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-13 00:06:19 +0200

    Update antiout.js

--- Priyansh/commands/antiout.js
+++ Priyansh/commands/antiout.js
@@ -1,22 +1,24 @@
 module.exports.config = {
-    name: "antiout",
+    name: "قفل",
     version: "1.0.0",
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     hasPermssion: 1,
-    description: "Turn off antiout",
+    description: "تشغيل أو ايقاف خاصية منع الخروج",
     usages: "antiout on/off",
-    commandCategory: "system",
+    commandCategory: "النظام",
     cooldowns: 0
 };
 
 module.exports.run = async({ api, event, Threads}) => {
     let data = (await Threads.getData(event.threadID)).data || {};
-    if (typeof data["antiout"] == "undefined" || data["antiout"] == false) data["antiout"] = true;
-    else data["antiout"] = false;
+    if (typeof data["antiout"] == "undefined" || data["antiout"] == false) {
+        data["antiout"] = true;
+    } else {
+        data["antiout"] = false;
+    }
     
     await Threads.setData(event.threadID, { data });
     global.data.threadData.set(parseInt(event.threadID), data);
     
-    return api.sendMessage(`✅ Done ${(data["antiout"] == true) ? "turn on" : "Turn off"} successful antiout!`, event.threadID);
-
-}
+    return api.sendMessage(`✅ تم ${(data["antiout"] == true) ? "تشغيل" : "ايقاف"} خاصية منع الخروج بنجاح!`, event.threadID);
+        }
