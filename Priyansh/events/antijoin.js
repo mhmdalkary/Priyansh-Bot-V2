ed3b68a86ec244dd02f4739a6091263597156aa8
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-12 17:35:21 +0200

    Update antijoin.js

--- Priyansh/events/antijoin.js
+++ Priyansh/events/antijoin.js
@@ -1,25 +1,34 @@
 module.exports.config = {
- name: "antijoin",
- eventType: ["log:subscribe"],
- version: "1.0.0",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
- description: "Welcome new members to the group"
+    name: "antijoin",
+    eventType: ["log:subscribe"],
+    version: "1.0.0",
+    credits: "Priyansh Rajput",
+    description: "منع الانضمام التلقائي للأعضاء الجدد"
 };
 
-module.exports.run = async function ({ event, api, Threads, Users }) {
- 	let data = (await Threads.getData(event.threadID)).data
- 	if (data.newMember == false) return;
- 	if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) return
-    else if(data.newMember == true) {
-    var memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId)
-			for (let idUser of memJoin) {
-					await new Promise(resolve => setTimeout(resolve, 1000));
-					api.removeUserFromGroup(idUser, event.threadID, async function (err) {
-                        if (err) return data["newMember"] = false;
-                            await Threads.setData(event.threadID, { data });
-                              global.data.threadData.set(event.threadID, data);
-                    })
-			}
- 	return api.sendMessage(`» Your team now has Anti Join mode turned on, please turn it off before adding a new 👻 member`, event.threadID);
- }
-}
+module.exports.run = async function ({ event, api, Threads }) {
+    let data = (await Threads.getData(event.threadID)).data;
+    if (data.newMember === false) return;
+    if (event.logMessageData.addedParticipants.some(i => i.userFbId === api.getCurrentUserID())) return;
+
+    if (data.newMember === true) {
+        const memJoin = event.logMessageData.addedParticipants.map(info => info.userFbId);
+
+        for (let idUser of memJoin) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            api.removeUserFromGroup(idUser, event.threadID, async function (err) {
+                if (err) {
+                    data["newMember"] = false;
+                    await Threads.setData(event.threadID, { data });
+                    global.data.threadData.set(event.threadID, data);
+                }
+            });
+        }
+
+        return api.sendMessage(
+`✧✦ 『 وضع منع الانضمام مفعّل 』 ✦✧
+➤⊹ يرجى تعطيل الوضع قبل إضافة أعضاء جدد.`,
+            event.threadID
+        );
+    }
+};
