4488f30e8454735f2d6c5635649a586a30d8af38
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-13 00:26:10 +0200

    Update autoreset.js

--- Priyansh/commands/autoreset.js
+++ Priyansh/commands/autoreset.js
@@ -4,22 +4,20 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "إعادة تشغيل تلقائية",
-    commandCategory: "النظام",
+    commandCategory: "System",
     cooldowns: 5
 }
 
-module.exports.handleEvent = async function({ api, event, args, Users, Threads }) {
+module.exports.handleEvent = async function({ api, event }) {
     const moment = require("moment-timezone");
     var timeNow = moment.tz("Asia/Riyadh").format("HH:mm:ss"); // توقيت السعودية
     var idad = global.config.ADMINBOT;    
-    console.log(timeNow)
     var seconds = moment.tz("Asia/Riyadh").format("ss");
 
     var times = [
-        `12:00:${seconds}`, `11:00:${seconds}`, `10:00:${seconds}`,
-        `09:00:${seconds}`, `08:00:${seconds}`, `07:00:${seconds}`,
-        `06:00:${seconds}`, `05:00:${seconds}`, `04:00:${seconds}`,
-        `03:00:${seconds}`, `02:00:${seconds}`, `01:00:${seconds}`
+        `00:00:${seconds}`, `04:00:${seconds}`, `06:00:${seconds}`,
+        `08:00:${seconds}`, `12:00:${seconds}`, `16:00:${seconds}`, 
+        `18:00:${seconds}`, `20:00:${seconds}`, `23:59:${seconds}`
     ];
 
     if (times.includes(timeNow) && seconds < 6) {
@@ -31,8 +29,9 @@ module.exports.handleEvent = async function({ api, event, args, Users, Threads }
     }
 }
 
-module.exports.run = async ({ api, event, args }) => {
+module.exports.run = async ({ api, event }) => {
     const moment = require("moment-timezone");
     var timeNow = moment.tz("Asia/Riyadh").format("HH:mm:ss"); // توقيت السعودية
     api.sendMessage(`الوقت الآن: ${timeNow}`, event.threadID);
+}    api.sendMessage(`الوقت الآن: ${timeNow}`, event.threadID);
 }
