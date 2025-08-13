5ad2cd7637be5e13e4ed544a4bcd0bb33c7aac4b
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-13 12:08:15 +0200

    Update altar.js

--- Priyansh/commands/altar.js
+++ Priyansh/commands/altar.js
@@ -1,23 +1,40 @@
-module.exports.config = {  
-	name: "ذبح",  
-	version: "1.0.2",  
-	hasPermssion: 0,  
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",  
-	description: "المذبح [مذكور]",  
-	commandCategory: "edit-img",  
-	usages: "[@وسم]",  
-	cooldowns: 5,  
-	dependencies: {  
-	  "fs-extra": "",  
-	  "axios": "",  
-	  "canvas" :"",  
-	  "jimp": "",  
-	  "node-superfetch": ""  
-	}  
-};  
-// ...
- api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "هاي، كيف حالك؟ :))"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);  
-// ...  
+module.exports.config = {
+	name: "ذبح",
+	version: "1.0.2",
+	hasPermssion: 0,
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	description: "altar [mentioned]",
+	commandCategory: "edit-image",
+	usages: "[@tag]",
+	cooldowns: 5,
+	dependencies: {
+	  "fs-extra": "",
+	  "axios": "",
+	  "canvas" :"",
+	  "jimp": "",
+	  "node-superfetch": ""
+	}
+};
+
+module.exports.circle = async (image) => {
+	  const jimp = global.nodemodule['jimp'];
+  	image = await jimp.read(image);
+  	image.circle();
+  	return await image.getBufferAsync("image/png");
+};
+
+module.exports.run = async ({ event, api, args, Users }) => {
+try {
+  const Canvas = global.nodemodule['canvas'];
+  const request = global.nodemodule["node-superfetch"];
+  const jimp = global.nodemodule["jimp"];
+  const fs = global.nodemodule["fs-extra"];
+  var path_toilet = __dirname+'/cache/bantho.png'; 
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  const canvas = Canvas.createCanvas(960, 634);
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('https://i.imgur.com/brK0Hbb.jpg');
+  
 	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
 	avatar = await this.circle(avatar.body);
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
