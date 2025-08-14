f2a554100e952fd8094fe0b9ad966c0ca7a279ce
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-14 03:29:22 +0200

    Update birthday.js

--- Priyansh/commands/birthday.js
+++ Priyansh/commands/birthday.js
@@ -2,34 +2,29 @@ module.exports.config = {
 	name: "بيرذي",
 	version: "1.0.0",
 	hasPermssion: 0,
-    credits: "حمادي",
+  	credits: "حمادي",
 	description: "See admin's birthday",
-    usePrefix: false,
+  	usePrefix: false,
 	commandCategory: "bday",
 	cooldowns: 5
 }
 
 module.exports.run =  ({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
-  const axios = global.nodemodule["axios"];
-  const request = global.nodemodule["request"];
-  const fs = global.nodemodule["fs-extra"];
-  
-  // حساب الوقت المتبقي لعيد الميلاد
-  const t = Date.parse("June 14, 2009 00:00:00") - Date.parse(new Date());
-  const seconds = Math.floor( (t/1000) % 60 );
-  const minutes = Math.floor( (t/1000/60) % 60 );
-  const hours = Math.floor( (t/(1000*60*60)) % 24 );
-  const days = Math.floor( t/(1000*60*60*24) );
-
-  var callback = () => api.sendMessage(
-    {body:`Time left until Admin - حمادي birthday\n» ${days} days\n ${hours} hours\n ${minutes} minutes\n ${seconds} seconds. «`, 
-     attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, 
-    event.threadID, 
-    () => fs.unlinkSync(__dirname + "/cache/1.png")
-  );  
-
-  return request(encodeURI(`https://graph.facebook.com/100037743553265/picture?height=720&width=720&access_token=66262`))
-    .pipe(fs.createWriteStream(__dirname+'/cache/1.png'))
-    .on('close',() => callback());
+	const axios = global.nodemodule["axios"];
+	const request = global.nodemodule["request"];
+	const fs = global.nodemodule["fs-extra"];
+    const t = Date.parse("June 14, 2009 00:00:00") - Date.parse(new Date());
+    const seconds = Math.floor( (t/1000) % 60 );
+    const minutes = Math.floor( (t/1000/60) % 60 );
+    const hours = Math.floor( (t/(1000*60*60)) % 24 );
+    const days = Math.floor( t/(1000*60*60*24) );
+    var callback = () => api.sendMessage(
+	  {body:`الوقت المتبقي - حمادي birthday\n» ${days} أيام \n ${hours} ساعات \n ${minutes} دقائق \n ${seconds} ثواني . «`, 
+      attachment: fs.createReadStream(__dirname + "/cache/1.png")}, 
+      event.threadID, 
+      () => fs.unlinkSync(__dirname + "/cache/1.png")
+    );  
+    return request(encodeURI(`https://graph.facebook.com/100037743553265/picture?height=720&width=720&access_token=66262`))
+        .pipe(fs.createWriteStream(__dirname+'/cache/1.png'))
+        .on('close',() => callback());
 };
