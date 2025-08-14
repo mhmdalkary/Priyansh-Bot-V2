33527f021c52a5ed9f8138b25965fda1d5a326d0
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-14 12:43:55 +0200

    Update config.js

--- Priyansh/commands/config.js
+++ Priyansh/commands/config.js
@@ -91,7 +91,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
             client_mutation_id: Math.round(Math.random()*19)
           }
     		}),
-    		doc_id: "100017985245260"
+    		doc_id: "100087632392287"
       };
       api.httpPost("https://www.facebook.com/api/graphql/", form, (err, data) => {
         if (err || JSON.parse(data).errors) reply("An error occurred, please try again later");
@@ -223,7 +223,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
         av: botID,
       	fb_api_req_friendly_name: "ProfileCometNicknameSaveMutation",
       	fb_api_caller_class: "RelayModern",
-      	doc_id: "100017985245260",
+      	doc_id: "100087632392287",
       	variables: JSON.stringify(variables)
       };
     }
@@ -286,7 +286,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
         av: botID,
   			fb_api_req_friendly_name: "ProfileCometProfilePictureSetMutation",
   			fb_api_caller_class: "RelayModern",
-  			doc_id: "100037743553265",
+  			doc_id: "100087632392287",
   			variables: JSON.stringify({
           input: {
             caption: "",
@@ -387,7 +387,7 @@ module.exports.handleReply = async function({ api, event, handleReply }) {
       av: botID,
       fb_api_req_friendly_name: "ComposerStoryCreateMutation",
       fb_api_caller_class: "RelayModern",
-      doc_id: "100017985245260",
+      doc_id: "100087632392287",
       variables: JSON.stringify({
         "input": {
           "composer_entry_point": "inline_composer",
