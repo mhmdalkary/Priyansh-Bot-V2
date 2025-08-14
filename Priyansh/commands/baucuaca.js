720af18ef5023611495da80d15907ee20af0178c
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-14 02:06:54 +0200

    Update baucuaca.js

--- Priyansh/commands/baucuaca.js
+++ Priyansh/commands/baucuaca.js
@@ -1,25 +1,25 @@
 module.exports.config = {
-	name: "baucuaca",
+	name: "رهان",
 	version: "0.0.1",
 	hasPermssion: 0,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Games",
-	commandCategory: "games",
+	description: "ألعاب",
+	commandCategory: "الألعاب",
 	usages: "baucuaca 500",
 	cooldowns: 5,
 };
 
 module.exports.run = async function({ api, event, args, Currencies }) {
             let { threadID, messageID, senderID } = event;
-            const slotItems = ["Vote","Crab","Fish"];
+            const slotItems = ["تصويت","سلطعون","سمكة"];
 			let money = (await Currencies.getData(event.senderID)).money;
 			var coin = args.join(" ");
-			if (!coin) return api.sendMessage(`You have not entered the bet amount!`, threadID, messageID);
+			if (!coin) return api.sendMessage(`لم تدخل مبلغ الرهان!`, threadID, messageID);
 			let win = false;
-			if (isNaN(coin)|| coin.indexOf("-") !== -1) return api.sendMessage(`Your bet amount is not a number, please review usage at ${prefix}help baucuaca`, threadID, messageID);
-			if (!coin) return api.sendMessage("The bet amount has not been entered!", threadID, messageID);
-			if (coin > money) return api.sendMessage(`Your amount is not enough`, threadID, messageID);
-			if (coin < 50) return api.sendMessage(`Your bet is too small, the minimum is 50$!`, threadID, messageID);
+			if (isNaN(coin)|| coin.indexOf("-") !== -1) return api.sendMessage(`مبلغ الرهان ليس رقماً، يرجى مراجعة الاستخدام على ${prefix}help baucuaca`, threadID, messageID);
+			if (!coin) return api.sendMessage("لم يتم إدخال مبلغ الرهان!", threadID, messageID);
+			if (coin > money) return api.sendMessage(`المبلغ لديك غير كافي`, threadID, messageID);
+			if (coin < 50) return api.sendMessage(`مبلغ الرهان صغير جداً، الحد الأدنى هو 50$!`, threadID, messageID);
 			let number = [];
 			for (i = 0; i < 3; i++) number[i] = Math.floor(Math.random() * slotItems.length);
 			if (number[0] == number[1] && number[1] == number[2]) {
@@ -30,5 +30,5 @@ module.exports.run = async function({ api, event, args, Currencies }) {
 					money *= 2;
 					win = true;
 				}
-				(win) ? api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nYou have won\nReceive ${coin} dollar.`, threadID, () => Currencies.increaseMoney(senderID, parseInt(coin)), messageID) : api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nYou have loss\nThe amount you bet belongs to the house`, threadID, () => Currencies.decreaseMoney(senderID, parseInt(coin)), messageID);
+				(win) ? api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nلقد فزت\nستستلم ${coin} دولار.`, threadID, () => Currencies.increaseMoney(senderID, parseInt(coin)), messageID) : api.sendMessage(`${slotItems[number[0]]} | ${slotItems[number[1]]} | ${slotItems[number[2]]}\nلقد خسرت\nالمبلغ الذي رهنته يذهب للبيت`, threadID, () => Currencies.decreaseMoney(senderID, parseInt(coin)), messageID);
 }
