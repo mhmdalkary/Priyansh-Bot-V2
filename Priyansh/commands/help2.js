d20fd44eb5dbf2d18256f98d53a98510a2e87c54
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-16 17:48:43 +0200

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -14,7 +14,7 @@ module.exports.config = {
 };
 
 module.exports.languages = {
-  "ar": {    
+  "ar": {
     "moduleInfo": "「 %1 」\n%2\n\n❯ الاستخدام: %3\n❯ الفئة: %4\n❯ وقت الانتظار: %5 ثانية\n❯ الصلاحية: %6\n\n» كود المودول بواسطة %7 «",    
     "helpList": '[ يوجد %1 أوامر في هذا البوت، استخدم: "%2help اسم الأمر" لمعرفة طريقة الاستخدام! ]',    
     "user": "مستخدم",    
@@ -33,7 +33,8 @@ module.exports.handleEvent = function ({ api, event, getText }) {
   const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};    
   const command = commands.get(splitBody[1].toLowerCase());    
   const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;    
-  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, 
+    ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
 }
 
 module.exports.run = function({ api, event, args, getText }) {
@@ -50,22 +51,22 @@ module.exports.run = function({ api, event, args, getText }) {
     const numberOfOnePage = 9999;    
     let i = 0;    
     let msg = "";    
-    
+
     for (var [name, value] of (commands)) {    
       name += ``;    
       arrayInfo.push(name);    
     }    
 
     arrayInfo.sort((a, b) => a.data - b.data);    
-    
-    const startSlice = numberOfOnePage*page - numberOfOnePage;    
+
+    const startSlice = numberOfOnePage * page - numberOfOnePage;    
     i = startSlice;    
     const returnArray = arrayInfo.slice(startSlice, startSlice + numberOfOnePage);    
-    
+
     for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;    
-    
-    const siu = `📄 قائمة الأوامر\nصنع بواسطة حمود 🥀\nلمزيد من المعلومات اكتب /help (اسم الأمر) ✨`;
-    const text = `\nالصفحة (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})`;
+
+    const siu = `قائمة الأوامر\nصنع بواسطة حمود\nلمزيد من المعلومات اكتب /help (اسم الأمر)`;
+    const text = `\nالصفحة (${page}/${Math.ceil(arrayInfo.length / numberOfOnePage)})`;
 
     return api.sendMessage(siu + "\n\n" + msg + text, threadID, async (error, info) => {    
       if (autoUnsend) {    
@@ -75,5 +76,6 @@ module.exports.run = function({ api, event, args, getText }) {
     }, event.messageID);    
   }    
 
-  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, 
+    ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
 };
