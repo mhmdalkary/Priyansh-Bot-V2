5e724d29bebb141b046a7ad7d6c74708e5290199
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-16 09:37:52 +0200

    Update and rename hugv3.js to hugv2.js

--- Priyansh/commands/hugv2.js
+++ Priyansh/commands/hugv2.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-    name: "hugv3",
+    name: "حضن2",
     version: "7.3.1",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "Hug 🥰",
+    description: "أرسل حضن 🥰",
     commandCategory: "img",
-    usages: "[@mention]",
+    usages: "[@تاغ] | الرد على رسالة",
     cooldowns: 5,
     dependencies: {
         "axios": "",
@@ -15,7 +15,7 @@ module.exports.config = {
     }
 };
 
-module.exports.onLoad = async() => {
+module.exports.onLoad = async () => {
     const { resolve } = global.nodemodule["path"];
     const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
     const { downloadFile } = global.utils;
@@ -28,33 +28,39 @@ module.exports.onLoad = async() => {
 async function makeImage({ one, two }) {
     const fs = global.nodemodule["fs-extra"];
     const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
+    const axios = global.nodemodule["axios"];
     const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let batgiam_img = await jimp.read(__root + "/hugv3.png");
-    let pathImg = __root + `/batman${one}_${two}.png`;
+    let base_img = await jimp.read(__root + "/hugv3.png");
+    let pathImg = __root + `/hug_${one}_${two}.png`;
+
+    // تحميل صورة المستخدم الأول
     let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-    
     let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
+
+    // تحميل صورة المستخدم الثاني
+    let avatarTwo = __root + `/avt_${two}.png`;
     let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
     fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
+
+    // تحويل الصور لدائرية
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(220, 220), 200, 50).composite(circleTwo.resize(220, 220), 490, 200);
-    
-    let raw = await batgiam_img.getBufferAsync("image/png");
-    
+
+    // دمج الصور على الخلفية
+    base_img.composite(circleOne.resize(150, 150), 320, 100).composite(circleTwo.resize(130, 130), 280, 280);
+
+    let raw = await base_img.getBufferAsync("image/png");
+
     fs.writeFileSync(pathImg, raw);
     fs.unlinkSync(avatarOne);
     fs.unlinkSync(avatarTwo);
-    
+
     return pathImg;
 }
+
 async function circle(image) {
     const jimp = require("jimp");
     image = await jimp.read(image);
@@ -62,13 +68,19 @@ async function circle(image) {
     return await image.getBufferAsync("image/png");
 }
 
-module.exports.run = async function ({ event, api, args }) {    
+module.exports.run = async function ({ event, api }) {
     const fs = global.nodemodule["fs-extra"];
     const { threadID, messageID, senderID } = event;
-    const mention = Object.keys(event.mentions);
-    if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
-    else {
-        const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
-    }
-      }
+
+    // التحقق من التاغ أو الرد
+    let mention = Object.keys(event.mentions);
+    let targetID;
+    if (mention[0]) targetID = mention[0];
+    else if (event.type === "message_reply") targetID = event.messageReply.senderID;
+    else return api.sendMessage("يرجى عمل تاغ أو الرد على رسالة الشخص الذي تريد حضنه 😔", threadID, messageID);
+
+    const one = senderID;
+    const two = targetID;
+
+    return makeImage({ one, two }).then(path => api.sendMessage({ body: "🥰 حضنك وصل!", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+}
